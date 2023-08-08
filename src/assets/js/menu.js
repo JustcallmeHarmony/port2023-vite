ee4e76162da75ee7b8657f5d7175d723b650ae20
Joon <wnsduq1130@naver.com>
2023-08-09 00:03:05 +0900

    3.헤더완료

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,11 @@
-export function link() {
-    console.log(link)
+const headerToggle = document.getElementById("headerToggle");
+const headerNav = document.querySelector(".header__nav");
+
+if (headerToggle) {
+    headerToggle.addEventListener("click", () => {
+    headerNav.classList.toggle("show");
+    headerToggle.getAttribute("aria-expanded") === "true"
+        ? headerToggle.setAttribute("aria-expanded", "false")
+        : headerToggle.setAttribute("aria-expanded", "true");
+    });
 }
